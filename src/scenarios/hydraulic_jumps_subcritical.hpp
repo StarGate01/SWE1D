8471847839ef5e1af6521e7da843c322cca8dedd
foxtrot <felix.wieser@tum.de>
2017-11-11 12:49:08 +0100

    Started to implement scenarios for hydraulic jumps

---
+++ src/scenarios/hydraulic_jumps_subcritical.hpp
@@ -0,0 +1,67 @@
+/**
+ * @file shockschock.h
+ * @brief Contains shock-shock riemann problems
+*/
+
+#ifndef SCENARIOS_HYDRAULIC_SUBCRITICAL_H_
+#define SCENARIOS_HYDRAULIC_SUBCRITICAL_H_
+
+#include "../types.hpp"
+
+namespace scenarios
+{
+
+	class ShockShock
+	{
+
+	private:
+		
+		/** Number of cells */
+		const unsigned int m_size;
+		/** Point of collision (here: in the middle) */
+		const unsigned int m_xdis = m_size/2;
+		/** initial water height at all cells */
+		const signed int m_height = 10;
+		/** initial water speed left of xdis */
+		const signed int m_leftSpeed = 50;
+		/** initial water speed right of xdis */
+		const signed int m_rightSpeed = -10;
+
+	public:
+		ShockShock(unsigned int size)
+			: m_size(size)
+		{
+		}
+
+		//TODO: Add bathymetry
+
+		/**
+		 * @return Initial water height at pos
+		 */
+		unsigned int getHeight(unsigned int pos)
+		{
+			//TODO: Change to correct height
+			return m_height;
+		}
+
+		/**
+		 * @return Initial water speed at pos
+		 */
+		signed int getSpeed(unsigned int pos)
+		{
+			return (pos >= 0 && pos <=25) ? 4.42 : 0;
+		}
+
+		/**
+		 * @return Cell size of one cell (= domain size/number of cells)
+		 */
+		T getCellSize()
+		{
+			return 1000.f / m_size;
+		}
+
+	};
+
+}
+
+#endif /* SCENARIOS_ShockShock_H_ */
